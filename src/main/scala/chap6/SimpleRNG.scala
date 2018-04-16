8fcd85fa98079aea7e861f391c600e2ca41d757e
camon <camon85@gmail.com>
2018-04-17 01:36:32 +0900

    챕터6 연습문제 풀이중

--- src/main/scala/chap6/SimpleRNG.scala
+++ src/main/scala/chap6/SimpleRNG.scala
@@ -69,6 +69,37 @@ object SimpleRNG {
 
   type Rand[+A] = RNG => (A, RNG)
 
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def nonNegativeEven: Rand[Int] =
+    map(nonNegativeInt)(i => i - i % 2)
+
+  def doubleByMap: Rand[Double] = {
+    map(nonNegativeInt)(d => d / (Int.MaxValue.toDouble + 1))
+  }
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, rng2) = ra(rng)
+      val (b, rng3) = rb(rng2)
+      (f(a, b), rng3)
+    }
+
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map2(ra, rb)((_, _))
+
+  def randIntDouble: Rand[(Int, Double)] = both(int, double)
+
+  def randDoubleInt: Rand[(Double,Int)] = both(double, int)
+
   def main(args: Array[String]): Unit = {
     println("== 연습문제 6.1 == nonNegativeInt ")
     println(nonNegativeInt(SimpleRNG(1)))
@@ -88,11 +119,28 @@ object SimpleRNG {
     println(ints(10)(SimpleRNG(1))._1)
     println(intsTailRecursive(10)(SimpleRNG(1))._1)
 
-    println("== 연습문제 6.5 ==  ")
+    println("== 연습문제 6.5 == map을 이용해서 double 구현 ")
+    println(doubleByMap(SimpleRNG(1))_1)
+
+    println("== 연습문제 6.6 == map2 구현 ")
+//    println(map2(nonNegativeEven, double)(_ + _))
+
+    println("== 연습문제 6.7 == sequence 구현 ")
     println()
 
-    println("== 연습문제 6.6 ==  ")
-    println()
+//    println("== 연습문제 6.8 == flatMpa을 구현하고 그것을 이용해서 nonNegativeLessThan 구현")
+//    println()
+//
+//    println("== 연습문제 6.9 == map과 map2를 flatMap을 이용해서 다시 구현하라")
+//    println()
+//
+//    println("== 연습문제 6.10 == unit, map, map2, flatMap, sequence를 일반화하라")
+//    println()
+//
+//    println("== 연습문제 6.11 == state automata 구현")
+//    println()
+
+
   }
 
 }
